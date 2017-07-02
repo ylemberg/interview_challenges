2b64a0d9d30b88e7bee174c595f53da66a28b764
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-01 19:55:26 -0700

    [feat] Implement is-BST-checker

---
+++ isBST.js
@@ -0,0 +1,72 @@
+// Reference: https://www.interviewcake.com/question/javascript/bst-checker
+
+class BinaryTreeNode {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value)
+    return this.left
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value)
+    return this.right
+  }
+}
+
+const isBst = root => {
+  const inOrderNodes = []
+  const stack = [root]
+  let curr = root
+
+  while (stack.length) {
+    if (curr) {
+      curr.left && stack.push(curr.left)
+      curr = curr.left
+    } else {
+      curr = stack.pop()
+      if (curr) {
+        inOrderNodes.push(curr.val)
+        curr.right && stack.push(curr.right)
+        curr = curr.right
+      }
+    }
+  }
+
+  let max = Number.NEGATIVE_INFINITY
+  for (let i = 0; i < inOrderNodes.length; i++) {
+    if (inOrderNodes[i] <= max) {
+      return false
+    }
+    max = inOrderNodes[i]
+  }
+  return true
+}
+
+const bst = new BinaryTreeNode(4)
+const firstLeft = bst.insertLeft(2)
+const firstRight = bst.insertRight(7)
+const secondLeft = firstLeft.insertLeft(1)
+const secondRight = firstLeft.insertRight(3)
+const thirdLeft = firstRight.insertLeft(6)
+const thirdRight = firstRight.insertRight(8)
+const fourthRight = thirdRight.insertRight(9)
+const fifthRight = fourthRight.insertRight(10)
+const fourthLeft = thirdLeft.insertLeft(5)
+console.log(isBst(bst))
+
+const bst2 = new BinaryTreeNode(7)
+const firstLeft2 = bst2.insertLeft(3)
+const firstRight2 = bst2.insertRight(13)
+const secondLeft2 = firstLeft2.insertLeft(1)
+const secondRight2 = firstLeft2.insertRight(5)
+const thirdLeft2 = firstRight2.insertLeft(11)
+const thirdRight2 = firstRight2.insertRight(15)
+const fourthRight2 = thirdRight2.insertRight(16)
+const fifthRight2 = fourthRight2.insertRight(19)
+const fourthLeft2 = thirdLeft2.insertLeft(9)
+console.log(isBst(bst2))
