ab8ea58ae13bd20f8621dd5d45bbff4ec5498d47
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-04 14:45:28 -0700

    [feat] Find rotation point

---
+++ findRotationPoint.js
@@ -0,0 +1,28 @@
+const findRotationPoint = arr => {
+  let lower = 0
+  let upper = arr.length
+  const first = arr[0]
+
+  while (lower < upper) {
+    const mpIdx = lower + Math.floor((upper - lower) / 2)
+    const mpVal = arr[mpIdx]
+
+    if (first > mpVal) {
+      upper = mpIdx
+    } else {
+      lower = mpIdx
+    }
+
+    if ((arr[mpIdx - 1] > mpVal) && (arr[mpIdx + 1] > mpVal)) {
+      return mpIdx
+    } else if ((mpIdx === arr.length - 1) && (arr[mpIdx - 1] > mpVal)) {
+      return mpIdx
+    }
+  }
+}
+
+console.log(findRotationPoint(['r', 's', 't', 'w', 'z', 'c', 'd', 'p']))
+console.log(findRotationPoint(['r', 'a', 'c', 'd']))
+console.log(findRotationPoint(['r', 'x', 'a', 'd']))
+console.log(findRotationPoint(['x', 'y', 'z', 'a']))
+console.log(findRotationPoint(['k', 'v', 'a', 'b', 'c', 'd', 'e', 'g', 'i']))
