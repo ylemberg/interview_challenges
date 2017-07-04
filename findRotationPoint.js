eb65541109a80b49ceafd6adf4b1bd409b9e13a0
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-07-04 14:49:43 -0700

    [fix] Consider edge case where there is no rotation point

--- findRotationPoint.js
+++ findRotationPoint.js
@@ -13,10 +13,12 @@ const findRotationPoint = arr => {
       lower = mpIdx
     }
 
-    if ((arr[mpIdx - 1] > mpVal) && (arr[mpIdx + 1] > mpVal)) {
+    if (arr[mpIdx - 1] > mpVal && arr[mpIdx + 1] > mpVal) {
       return mpIdx
-    } else if ((mpIdx === arr.length - 1) && (arr[mpIdx - 1] > mpVal)) {
+    } else if (mpIdx === arr.length - 1 && arr[mpIdx - 1] > mpVal) {
       return mpIdx
+    } else if (mpIdx === arr.length - 1 && arr[mpIdx - 1] < mpVal) {
+      return 0
     }
   }
 }
@@ -25,4 +27,6 @@ console.log(findRotationPoint(['r', 's', 't', 'w', 'z', 'c', 'd', 'p']))
 console.log(findRotationPoint(['r', 'a', 'c', 'd']))
 console.log(findRotationPoint(['r', 'x', 'a', 'd']))
 console.log(findRotationPoint(['x', 'y', 'z', 'a']))
+console.log(findRotationPoint(['x', 'a']))
 console.log(findRotationPoint(['k', 'v', 'a', 'b', 'c', 'd', 'e', 'g', 'i']))
+console.log(findRotationPoint(['a', 'b', 'c', 'd', 'e']))
