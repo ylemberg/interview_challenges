0176ec14914b9ae058893397605215e5cfd7645b
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-12-02 18:31:58 -0800

    Implement validBrackets algorithm

---
+++ validBrackets.js
@@ -0,0 +1,34 @@
+const validBrackets = str => {
+  const validOpeners = {
+    '{': true,
+    '(': true,
+    '[': true
+  }
+  const validClosers = {
+    '}': '{',
+    ')': '(',
+    ']': '['
+  }
+  const openers = []
+
+  for (let i = 0; i < str.length; i++) {
+    if (validOpeners[str[i]]) {
+      openers.push(str[i])
+    } else if (validClosers[str[i]] && openers[openers.length - 1] !== validClosers[str[i]]) {
+      return false
+    } else if (validClosers[str[i]] && openers[openers.length - 1] === validClosers[str[i]]) {
+      openers.pop()
+    }
+  }
+
+  return !openers.length
+}
+
+console.log(validBrackets('{ [ ] ( ) }'))
+console.log(validBrackets('{ [ ( ] ) }'))
+console.log(validBrackets('(((({[([[[[((()))]]]])]}))))'))
+console.log(validBrackets('('))
+console.log(validBrackets(''))
+console.log(validBrackets('}'))
+console.log(validBrackets('{ [ }'))
+console.log(validBrackets('{ [ ( ] ) }'))
